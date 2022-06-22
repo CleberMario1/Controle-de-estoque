4711be4d9cc825c92030258acfcadfcd196dcf5b
CleberMario1 <juniorcardoso50@gmail.com>
2022-06-21 21:05:35 -0300

    ui

--- viewvendedor/cadastrar.php
+++ viewvendedor/cadastrar.php
@@ -9,6 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" type="text/css">
 </head>
 <body>
+<button type="button" class="btn btn-primary">Adicionar</button>
     <h1>morreu</h1>
     
 </body>
